afc73ec525e7c6efcf3b5ed08db0859f6f3d313d
TobiasRW <tobiasrw98@gmail.com>
2023-05-29 09:16:03 +0200

    submenu

--- scripts/burgermenu.js
+++ scripts/burgermenu.js
@@ -10,16 +10,18 @@ dropdown.addEventListener("click", () => {
 const produkterLink = document.querySelector('.nav-item > a');
 const submenu = document.querySelector('.submenu');
 
-// produkterLink.addEventListener('click', function (event) {
-//     event.preventDefault();
-//     submenu.style.display = submenu.style.display === 'none' ? 'flex' : 'none';
-// });
+// const submenuMobil = document.getElementById('submenu-mobile')
 
+//  produkterLink.addEventListener('click', function (event) {
+//      event.preventDefault();
+//      submenuMobil.style.display = submenuMobil.style.display === 'none' ? 'flex' : 'none';
+// });
 
 
 
 produkterLink.addEventListener('mouseenter', () => {
   submenu.style.display = 'flex';
+
 });
 
 produkterLink.addEventListener('mouseleave', () => {
