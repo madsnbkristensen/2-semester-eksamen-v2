d44e488faad38ff088c4ef201f0c1bf3d05d5f26
madsnbkristensen <madskristensen@hotmail.dk>
2023-06-12 13:47:01 +0200

    d

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -1,9 +1,9 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxV1ErjgfnY1TygPAnkfQRzawPLdvoQQy-5Ryj7jRFxTp54bAbqnMOPnX7J_O4B7TIT/exec'
-  const form = document.forms['submit-to-google-sheet']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxV1ErjgfnY1TygPAnkfQRzawPLdvoQQy-5Ryj7jRFxTp54bAbqnMOPnX7J_O4B7TIT/exec';
+const emailForm = document.getElementById('emailForm');
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+emailForm.addEventListener('submit', e => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(emailForm) })
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message));
+});
